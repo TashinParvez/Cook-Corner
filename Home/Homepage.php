ba42f2cd314afaa1a7ce70b5e1b6bf6391b44ba3
Mahbubur Rahman <mrahman623a@gmail.com>
2024-09-15 13:10:06 +0600

    feat:Navbar update

--- Home/Homepage.php
+++ Home/Homepage.php
@@ -16,8 +16,8 @@
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- css  -->
+    <link rel="stylesheet" href="../Includes/Navbar/navbarMain.css">
     <link href="Homepage.css" rel="stylesheet" type="text/css">
-
 </head>
 
 <body>
