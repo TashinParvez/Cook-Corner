b1be39f22d22330aee81a1ee01f5448f8acd58fb
Tashin Parvez <tashinparvez2002@gmail.com>
2024-09-21 02:41:47 +0600

    feat: added add recipe page basic

--- All Categories/allCategories.php
+++ All Categories/allCategories.php
@@ -44,6 +44,9 @@ mysqli_close($conn);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
+    <!-- favicon -->
+    <link rel="icon" href="../Images/logo/fav-icon.png" />
+
     <!-- css  -->
     <link rel="stylesheet" href="../Includes/Navbar/navbarMain.css"> <!-- Navbar CSS -->
     <link rel="stylesheet" href="CSS/allCategories.css">
