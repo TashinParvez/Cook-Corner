e8686db6d3291cb77e6a011761371f8d939f0314
Tashin Parvez <tashinparvez2002@gmail.com>
2024-09-25 21:47:24 +0600

    feat: mod recipe view

--- Recipe View/recipeView.php
+++ Recipe View/recipeView.php
@@ -12,8 +12,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-    <!-- css  -->
+    <!-- favicon -->
+    <link rel="icon" href="../Images/logo/fav-icon.png" />
 
+    <!-- css  -->
     <link rel="stylesheet" href="../Includes/Navbar/navbarMain.css"> <!-- Navbar CSS -->
 
 
@@ -409,8 +411,8 @@
 
     <!-- ============================== Footer ==================================== -->
     <?php
-    // include('../Includes/Footer/footermain.php');  // tashin
-    include('../Includes/Footer/footermainTry.php');  // tashin
+    include('../Includes/Footer/footermain.php');  // tashin
+    // include('../Includes/Footer/footermainTry.php');  // tashin
     ?>
     <!-- ============================== Footer End ==================================== -->
 
